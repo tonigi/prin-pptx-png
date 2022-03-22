c56e1f36808a995821261863d30b06ef95d30405
Toni <toni.giorgino@gmail.com>
2022-03-22 15:42:55 +0100

    nn

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,12 @@ uf=st.file_uploader("Upload a PPTX file. For now 1st slide only, sorry.",
 if uf is not None:
     bd=uf.getvalue()
     #px=tempfile.NamedTemporaryFile(delete=False)
-    px=open(tempfile.TemporaryDirectory().name+os.path.basename(uf.name),"wb")
+    outdir = tempfile.TemporaryDirectory().name
+    px=open(outdir+os.path.basename(uf.name),"wb")
     #px=open(os.path.basename(uf.name),"wb")
     px.write(bd)
     px.close()
-    os.system(f"soffice --headless --convert-to pdf {px.name}")
+    os.system(f"soffice --headless --outdir {outdir} --convert-to pdf {px.name}")
     pdf_name=px.name.replace(".pptx",".pdf")
     png_name=px.name.replace(".pptx",".png")
     os.system(f"convert -geometry 680x -depth 8 {pdf_name} {png_name}")
