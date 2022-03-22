3b6b5ea6b09aadf9f44980064b3c823d4a8a0d32
Toni <toni.giorgino@gmail.com>
2022-03-22 15:44:37 +0100

    nn

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if uf is not None:
     #px=open(os.path.basename(uf.name),"wb")
     px.write(bd)
     px.close()
-    os.system(f"soffice --headless --outdir {outdir} --convert-to pdf {px.name}")
+    os.system(f"soffice --headless  --convert-to pdf --outdir {outdir} {px.name}")
     pdf_name=px.name.replace(".pptx",".pdf")
     png_name=px.name.replace(".pptx",".png")
     os.system(f"convert -geometry 680x -depth 8 {pdf_name} {png_name}")
